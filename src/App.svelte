a95b6a9f0b8e37a21de6e024c18f5eeee75915ab
leonkalema <tullapeople@gmail.com>
2023-03-16 06:01:44 +0100

    design with google material

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,125 @@
+<!-- src/LinkRedirector.svelte -->
 <script>
-	export let name;
+  import { onMount } from 'svelte';
+  import { writable, derived } from 'svelte/store';
+
+  let websiteUrl = '';
+  let appleStoreUrl = '';
+  let androidStoreUrl = '';
+  const error = writable('');
+
+  const showError = derived(error, ($error) => $error !== '');
+
+  const isValidUrl = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    error.set('');
+
+    if (!isValidUrl(websiteUrl) || !isValidUrl(appleStoreUrl) || !isValidUrl(androidStoreUrl)) {
+      error.set('Please provide valid URLs for all fields.');
+      return;
+    }
+
+    const payload = { websiteUrl, appleStoreUrl, androidStoreUrl };
+    const response = await fetch('https://shy-tan-basket-clam-veil.cyclic.app/generate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // Display or handle the generated URL here
+      console.log('Generated URL:', data.generatedUrl);
+    } else {
+      error.set('Error generating URL. Please try again.');
+    }
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  /* Google-inspired Material Design styles */
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  form {
+    background-color: white;
+    max-width: 500px;
+    margin: 40px auto;
+    padding: 32px;
+    border-radius: 8px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
+
+  label {
+    display: block;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 8px;
+  }
+
+  input[type='url'] {
+    width: 100%;
+    font-size: 16px;
+    padding: 8px;
+    margin-bottom: 16px;
+    border: none;
+    border-bottom: 2px solid #4a4a4a;
+    outline: none;
+    transition: border-color 0.3s;
+  }
+
+  input[type='url']:focus {
+    border-bottom-color: #1a73e8;
+  }
+
+  button {
+    background-color: #1a73e8;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 8px 16px;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  button:hover {
+    background-color: #1565c0;
+  }
+
+  p {
+    color: #d32f2f;
+    font-weight: 500;
+  }
 </style>
+
+<form on:submit={handleSubmit}>
+  <div>
+    <label for="websiteUrl">Website URL:</label>
+    <input type="url" id="websiteUrl" bind:value={websiteUrl} required />
+  </div>
+  <div>
+    <label for="appleStoreUrl">Apple Store URL:</label>
+    <input type="url" id="appleStoreUrl" bind:value={appleStoreUrl} required />
+  </div>
+  <div>
+    <label for="androidStoreUrl">Android Store URL:</label>
+    <input type="url" id="androidStoreUrl" bind:value={androidStoreUrl} required />
+  </div>
+  <button type="submit">Submit</button>
+  {#if $showError}
+    <p>{$error}</p>
+  {/if}
+</form>
